bda28ac718296f3a2f6deef111f4c45cdac6036f
James Pickering <james_pic@hotmail.com>
2018-05-18 00:09:21 +0100

    Move config injection earlier - helps with (but doesn't yet fix) #1

--- content.js
+++ content.js
@@ -2,13 +2,12 @@ import * as defaultConfig from './default-config.json'
 // Inject inpage script
 var s = document.createElement('script')
 s.src = chrome.extension.getURL('inpage.js')
-s.onload = function() { this.remove() }
-document.documentElement.appendChild(s)
-
-window.addEventListener('load', () => {
+s.onload = function() {
+  this.remove()
   chrome.storage.local.get({spacesuitConfig: defaultConfig}, (config) => {
     var evt = document.createEvent("CustomEvent")
     evt.initCustomEvent("configureSpacesuit", true, true, config.spacesuitConfig)
     document.dispatchEvent(evt)
   })
-})
+}
+document.documentElement.appendChild(s)
